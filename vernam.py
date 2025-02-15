a77ecb3e6c211ec8384b452397a427ed304eaf5f
nothingdev-hub <sqbusters42@gmail.com>
2025-02-15 04:35:34 +0300

    vernam.py

--- vernam.py
+++ vernam.py
@@ -19,17 +19,24 @@ def xor(n1, n2):
     else:
         return n3   
 for i in iter(int, 1):
-    c = xor(input('Message: '), input('Key: '))
-    print('Result =', c)
-for i in iter(int, 1):
     op = input("1 - Encrypt, 2 - Decrypt ")
     if op == '1':
-        c = xor(input('Message: '), input('Key: '))
-        print('Result =', c)
+        keytype = input('1 - your key, 2 - random key ')
+        if keytype == '1':
+            c = xor(input('Message: '), input('Key: '))
+            print('Result =', c)
+        elif keytype == '2':
+            randomkey = ''
+            message = input('Message: ')
+            for i in range(len(message)):
+                randomkey += str(random.randint(0, 1))
+            c = xor(message, randomkey)
+            print('Key = ', randomkey)
+            print(c)
     elif op == '2':
         c = xor(input('Cipher-text: '), input('Key: '))
         print('Result =', c)
-            
+        
     
     
 
