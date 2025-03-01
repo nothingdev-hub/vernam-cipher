0c50d282d4cd02d7e4bc05feaaf2f8031cc58fbb
nothingdev-hub <sqbusters42@gmail.com>
2025-03-01 07:58:09 +0300

    vernam.py

--- vernam.py
+++ vernam.py
@@ -18,25 +18,7 @@ def xor(n1, n2):
     if n3 == '': #Если шифрованное или дешифрованное сообщение пустое - одно из условий не выполнилось
         return 'Error'
     else:
-        return n3   
-for i in iter(int, 1):
-    op = input("1 - Encrypt, 2 - Decrypt ")
-    if op == '1':
-        keytype = input('1 - your key, 2 - random key ')
-        if keytype == '1':
-            c = xor(input('Message: '), input('Key: '))
-            print('Result =', c)
-        elif keytype == '2':
-            randomkey = ''
-            message = input('Message: ')
-            for i in range(len(message)):
-                randomkey += str(random.randint(0, 1))
-            c = xor(message, randomkey)
-            print('Key = ', randomkey)
-            print('Result = ', c)
-    elif op == '2':
-        c = xor(input('Cipher-text: '), input('Key: '))
-        print('Result =', c)
+        return n3
         
     
     
